8f4da056a80857a882e311e1d918f9f8b935ec64
Mahmoud Dello <mahmoddello68@gmail.com>
2023-11-26 11:36:49 +0300

    :recycle: Small code fixes.
    Line sepraters and some grammer correction.

--- 02 - Intermdiate/Day 26 - Comprehensions/NATO-alphabet-start/main.py
+++ 02 - Intermdiate/Day 26 - Comprehensions/NATO-alphabet-start/main.py
@@ -1,23 +1,21 @@
 import pandas as pd
 
-# TODO 1. Create a dictionary in this format:
 
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for _, row in df.iterrows()}
 print(phonetic_dict)
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 word = input("Name : ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
 # each letter in the word is the key to access to the corresponding value
 print(output_list)
 
-# phonetic_dictionary["letter"] will look for a key called "letter" and return the value at that key
+# Phonetic_dictionary["letter"] will look for a key called "letter" and return the value at that key
 # (if it exists).
 #
-# But in the code output = [phonetic_dictionary[letter] for letter in word],
-# we are using letter as a variable.
+# But in the code output = [phonetic_dictionary[letter] for a letter in word],
+# we are using a letter as a variable.
 #
 # Your NATO dictionary looks like this:
 #
@@ -29,7 +27,7 @@ print(output_list)
 # }
 # Let's say word = "cab".
 #
-# In our code, output = [phonetic_dictionary[letter] for letter in word]
+# In our code, output = [phonetic_dictionary[letter] for a letter in word]
 #
 # letter first takes on the value "c". The first value of the output list will be
 #
@@ -37,7 +35,7 @@ print(output_list)
 #
 # So now output = ["Charlie"]
 #
-# Now we get the next letter in word. letter now has the value "a",
+# Now we get the next letter in word. Letter now has the value "a",
 # and the next value of the output list will be
 #
 # phonetic_dictionary[letter] = phonetic_dictionary["a"] = "Alfa"
